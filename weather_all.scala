0f86407524a4ec342fffaf3c3a5c5106e5bc254d
OlehMazur <olegmazurbidev@gmail.com>
2019-11-21 16:07:43 +0200

    weather_all commit

--- weather_all.scala
+++ weather_all.scala
@@ -1,4 +1,21 @@
 // Databricks notebook source
+//Type of ETL: 0 (only Baltika ) 1 (only CAP) 2 (Both)
+
+// COMMAND ----------
+
+val type_of_ETL: Int = 1
+
+// COMMAND ----------
+
+//Type of scenario: 0 (full weather extraction  ) , 1 (incremental weather extraction )
+
+// COMMAND ----------
+
+val type_of_scenario: Int = 1
+
+// COMMAND ----------
+
+//Configuration (Baltika)
 val storage_account_name = "staeeprodbigdataml2c"
 val storage_account_access_key = "EHYumrwso4XLSUHpvLptI33z7mumiZwZOErjrlP8FiW51Bb6NS2PaWJsqW9hsMttbZizgQjUexFZfZDBQJebYw=="
 spark.conf.set(
@@ -7,16 +24,41 @@ spark.conf.set(
 
 // COMMAND ----------
 
+//Configuration (CAP)
+spark.conf.set(
+  "fs.azure.sas.dcd.prdcbwesa01.blob.core.windows.net",
+  "https://prdcbwesa01.blob.core.windows.net/dcd?st=2019-09-13T15%3A01%3A24Z&se=2020-03-14T14%3A01%3A00Z&sp=rwdl&sv=2018-03-28&sr=c&sig=aErgDFXTRr3Lj519B4ZtjDHTp%2F3xsXchFqVuS2IAnGc%3D")
+
+// COMMAND ----------
+
+val current_year = "2019"
+
+// COMMAND ----------
+
+//constants
+
+// COMMAND ----------
+
 val readPath = "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/test_res.csv"
 val writePath = "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/ETL/Result" 
-//val result =  "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/Transformation/Weather"
+val writePath_СAP = "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/export_to_CAP"
 val SearchPath = "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/Transformation/Weather/"
 val file_type = "csv"
 val fname = "weather_ru.csv"
 
 // COMMAND ----------
 
-val year_list : Seq[String] = Seq("2016","2017","2018","2019","2020")
+//constants (CAP)
+
+// COMMAND ----------
+
+val writePath_GBS = "wasbs://dcd@prdcbwesa01.blob.core.windows.net/RU" 
+val readPath_GBS = "wasbs://dcd@prdcbwesa01.blob.core.windows.net/RU/ru_tmp" 
+
+// COMMAND ----------
+
+val year_list : Seq[String] = Seq("2019")
+//Seq("2016","2017","2018","2019","2020")
 
 // COMMAND ----------
 
@@ -48,20 +90,23 @@ city_plant.createOrReplaceTempView("city_plant")
 
 // COMMAND ----------
 
-city_list.collect.foreach {
- (x) => { val status = dbutils.notebook.run("weather_dark_sky", 
-                             0, 
-                             Map(
-                                 "City" -> x.toString().replace("[", "").replace("]", ""),
-                                 "Year" -> "2017",
-                                 "API_Key" -> "d4b20ef8a9ad8bcf2449be822fba03a4",
-                                 "CONTAINER_NAME"  -> "prod"     
-                                )
-                            ) 
-                println(status)
-                }
-  
+if (type_of_scenario == 0) 
+{
+    city_list.collect.foreach {
+     (x) => { val status = dbutils.notebook.run("weather_dark_sky", 
+                                 0, 
+                                 Map(
+                                     "City" -> x.toString().replace("[", "").replace("]", ""),
+                                     "Year" -> current_year,
+                                     "API_Key" -> "d4b20ef8a9ad8bcf2449be822fba03a4",
+                                     "CONTAINER_NAME"  -> "prod"     
+                                    )
+                                ) 
+                    println(status)
+                    }
+    }
 }
+
 
 // COMMAND ----------
 
@@ -118,44 +163,49 @@ result_df.createOrReplaceTempView("main")
 
 // COMMAND ----------
 
-val year_list : Seq[String] = Seq("2016","2017","2018","2019","2020")
+// %sql 
+// select distinct year(date) from main 
 
 // COMMAND ----------
 
-var i:Long = 0
-for (year <- year_list) {
- var g:Long = 0
-  val name : String = year  
-  val file_list : Seq[String] = dbutils.fs.ls(SearchPath).map(_.path).filter(_.contains(name))
-  for (file <- file_list)  {
-    val file_list : Seq[String] = dbutils.fs.ls(file).map(_.path).filter(_.contains(name)) 
-    if (file_list.size > 0) {
+//val year_list : Seq[String] = Seq("2016","2017","2018","2019","2020")
+
+// COMMAND ----------
+
+// var i:Long = 0
+// for (year <- year_list) {
+//  var g:Long = 0
+//   val name : String = year  
+//   val file_list : Seq[String] = dbutils.fs.ls(SearchPath).map(_.path).filter(_.contains(name))
+//   for (file <- file_list)  {
+//     val file_list : Seq[String] = dbutils.fs.ls(file).map(_.path).filter(_.contains(name)) 
+//     if (file_list.size > 0) {
  
-     var city = file_list(0).replace(file, "").replace("weather_", "").replace("_"+name + ".csv", "") 
-     var file_location = file + file_list(0).replace(file, "")
-     var dff = spark.read.format(file_type).option("delimiter", ";").option("header", "true").load(file_location) 
-      //println (year)
-      //println(dff.count)
-      var y = dff.count
-     i = i+ y
-    }
+//      var city = file_list(0).replace(file, "").replace("weather_", "").replace("_"+name + ".csv", "") 
+//      var file_location = file + file_list(0).replace(file, "")
+//      var dff = spark.read.format(file_type).option("delimiter", ";").option("header", "true").load(file_location) 
+//       //println (year)
+//       //println(dff.count)
+//       var y = dff.count
+//      i = i+ y
+//     }
      
-  }
+//   }
  
-println(year)
-println(g)
-}
- println (i) 
+// println(year)
+// println(g)
+// }
+//  println (i) 
  
 
 // COMMAND ----------
 
-// MAGIC %sql
-// MAGIC select  year(date), count(*)  from main group by year(date) --order by 1 --where city = 'Irkutsk' --and date <> '2015-12-31'
+// %sql
+// select  year(date), count(*)  from main group by year(date) --order by 1 --where city = 'Irkutsk' --and date <> '2015-12-31'
 
 // COMMAND ----------
 
-// MAGIC %sql select count(*) from main
+// %sql select count(*) from main
 
 // COMMAND ----------
 
@@ -163,35 +213,103 @@ println(g)
 
 // COMMAND ----------
 
-val sql_qury = """
-select distinct m.city, c.plant, cl.weekid calendar_yearweek, date time, m.apparentTemperatureMax, m.cloudCover, m.humidity, m.windSpeed
+val sql_query = """
+select distinct cl.MonthId partition_name,  m.city, c.plant, cl.weekid calendar_yearweek, date time, m.apparentTemperatureMax, m.cloudCover, m.humidity, m.windSpeed
 from main m left join city_plant c  on  m.city = c.city left join calendar cl on cast(m.date as timestamp)  = cl.DayName
 """
 
 // COMMAND ----------
 
+val sqldf = spark.sql(sql_query)
+
+// COMMAND ----------
+
+//Load hole file to Baltika
+
+// COMMAND ----------
+
+// import com.databricks.WorkflowException
+// import java.io.FileNotFoundException
+
+// var Result = "Failure"   
+
+// try {
+ 
+// val sql_df = spark.sql(sql_query)
+// sql_df.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", ";").save(readPath)
+
+// val name : String = "part-00000"  
+// val file_list : Seq[String] = dbutils.fs.ls(readPath).map(_.path).filter(_.contains(name))
+// val read_name = if (file_list.length >= 1 ) file_list(0).replace(readPath + "/", "")
+// val row_count = spark.read.format("csv").option("inferSchema", "true").option("delimiter", ";").option("header", "true").load(file_list(0)).count   
+// dbutils.fs.mv(readPath+"/"+read_name , writePath+"/"+fname)   
+// dbutils.fs.rm(readPath , recurse = true) 
+// if (row_count > 0) Result = "Success" else println("The file " +writePath+"/"+fname + " is empty !" )
+// } 
+// catch {
+//   case e:FileNotFoundException => println("Error, " + e)
+//   case e:WorkflowException  => println("Error, " + e)
+// }
+
+// dbutils.notebook.exit(Result)
+
+
+
+// COMMAND ----------
+
+def exportToBlobStorage (type_of_ETL:Int): String = { 
+
 import com.databricks.WorkflowException
 import java.io.FileNotFoundException
 
-var Result = "Failure"   
+var Result = "Failure" 
+val partition_field = "partition_name"
+val export_format = "com.databricks.spark.csv"
+val export_delimiter = Character.toString(7.toChar)
+
+var readPath_ETL = if (type_of_ETL == 0) readPath else if (type_of_ETL == 1) readPath_GBS else null
+var writePath_ETL= if (type_of_ETL == 0) writePath_СAP else if (type_of_ETL == 1) writePath_GBS else null
 
 try {
- 
-val sql_df = spark.sql(sql_qury)
-sql_df.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", ";").save(readPath)
+  sqldf
+  .coalesce(1)
+  .write.mode("overwrite")
+  .format(export_format)
+  .option("header", "true")
+  .option("inferSchema", "true")
+  .option("delimiter", export_delimiter)
+  .partitionBy(partition_field)
+  .save(readPath_ETL)
 
-val name : String = "part-00000"  
-val file_list : Seq[String] = dbutils.fs.ls(readPath).map(_.path).filter(_.contains(name))
-val read_name = if (file_list.length >= 1 ) file_list(0).replace(readPath + "/", "")
-val row_count = spark.read.format("csv").option("inferSchema", "true").option("delimiter", ";").option("header", "true").load(file_list(0)).count   
-dbutils.fs.mv(readPath+"/"+read_name , writePath+"/"+fname)   
-dbutils.fs.rm(readPath , recurse = true) 
-if (row_count > 0) Result = "Success" else println("The file " +writePath+"/"+fname + " is empty !" )
-} 
+  val name : String = "part-00000"   
+  val path_list : Seq[String] = dbutils.fs.ls(readPath_ETL).map(_.path).filter(_.contains(partition_field))
+
+  for (path <- path_list) {
+   var partition_name = path.replace(readPath_ETL + "/" + partition_field + "=", "").replace("/", "")
+   var file_list : Seq[String] = dbutils.fs.ls(path).map(_.path).filter(_.contains(name)) 
+   var read_name =  if (file_list.length >= 1 ) file_list(0).replace(path + "/", "") 
+   var fname = "WEATHERDIRECT_" + partition_name + "_RU_DCD"+ ".csv" 
+   dbutils.fs.mv(read_name.toString , writePath_ETL+"/"+fname) 
+    }
+  dbutils.fs.rm(readPath_ETL , recurse = true) 
+  Result = "Success" 
+  } 
 catch {
-  case e:FileNotFoundException => println("Error, " + e)
-  case e:WorkflowException  => println("Error, " + e)
+    case e:FileNotFoundException => println("Error, " + e)
+    case e:WorkflowException  => println("Error, " + e)
+  }
+
+  Result
 }
 
-dbutils.notebook.exit(Result)
 
+
+// COMMAND ----------
+
+val Result = 
+if (type_of_ETL == 0) exportToBlobStorage(0) 
+else if (type_of_ETL == 1) exportToBlobStorage(1)
+else if (type_of_ETL == 2) {exportToBlobStorage(0); exportToBlobStorage(1) }
+else "Unexpected parameter"
+
+dbutils.notebook.exit(Result)
