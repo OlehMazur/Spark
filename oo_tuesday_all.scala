6a0ea7f5641ba7d3ded27e08e7cdfdf245b35b30
OlehMazur <olegmazurbidev@gmail.com>
2019-12-05 15:52:07 +0200

    final etl notebook

--- oo_tuesday_all.scala
+++ oo_tuesday_all.scala
@@ -1,10 +1,42 @@
 // Databricks notebook source
-//Configuration
+//Type of ETL: 0 (only Baltika ) 1 (only CAP) 2 (Both)
+
+// COMMAND ----------
+
+val type_of_ETL: Int = 0
+
+// COMMAND ----------
+
+//Type of data extraction: 0 (full) , 1 (incremental )
+
+// COMMAND ----------
+
+val type_of_data_extract: Int = 1
+
+// COMMAND ----------
+
+// The range of month in case of incremental loading (only if type_of_data_extract = 1 !!! )
+
+// COMMAND ----------
+
+val num_of_days_before_current_date: Int = 30 //number of day before current date
+val num_of_days_after_current_date: Int = 30  //number of day after current date
+
+// COMMAND ----------
+
+//Configuration (Baltika)
 val storage_account_name = "staeeprodbigdataml2c"
 val storage_account_access_key = "EHYumrwso4XLSUHpvLptI33z7mumiZwZOErjrlP8FiW51Bb6NS2PaWJsqW9hsMttbZizgQjUexFZfZDBQJebYw=="
 spark.conf.set(
   "fs.azure.account.key."+storage_account_name+".blob.core.windows.net",
   storage_account_access_key)
+
+// COMMAND ----------
+
+//Configuration (CAP)
+spark.conf.set(
+  "fs.azure.sas.dcd.prdcbwesa01.blob.core.windows.net",
+  "https://prdcbwesa01.blob.core.windows.net/dcd?st=2019-09-13T15%3A01%3A24Z&se=2020-03-14T14%3A01%3A00Z&sp=rwdl&sv=2018-03-28&sr=c&sig=aErgDFXTRr3Lj519B4ZtjDHTp%2F3xsXchFqVuS2IAnGc%3D")
 
 // COMMAND ----------
 
@@ -14,7 +46,17 @@ spark.conf.set(
 
 val readPath = "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/test_res.csv"
 val writePath = "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/ETL/Result" //ETL/Result //etl_fbkp
+val writePath_СAP = "wasbs://prod@staeeprodbigdataml2c.blob.core.windows.net/export_to_CAP"
 val fname = "open_orders_etl_tuesday_with_format_All.csv"
+
+// COMMAND ----------
+
+//constants (CAP)
+
+// COMMAND ----------
+
+val writePath_GBS = "wasbs://dcd@prdcbwesa01.blob.core.windows.net/RU" 
+val readPath_GBS = "wasbs://dcd@prdcbwesa01.blob.core.windows.net/RU/ru_tmp" 
 
 // COMMAND ----------
 
@@ -152,9 +194,21 @@ sqldf.createOrReplaceTempView("result_source_table")
 
 // COMMAND ----------
 
-//result export
+//result export to ETl\Result
 
 // COMMAND ----------
+
+val query_bal =  """
+  select distinct * from result_source_table 
+  """
+
+// COMMAND ----------
+
+val sqldf_bal = spark.sql(query_bal)
+
+// COMMAND ----------
+
+def exportToBlobStorage_Baltika: String = { 
 
 import com.databricks.WorkflowException
 import java.io.FileNotFoundException
@@ -162,12 +216,7 @@ import java.io.FileNotFoundException
 var Result = "Failure"   
 
 try {
-val sqldf = spark.sql(
-  """
-  select distinct * from result_source_table 
-  """
-  )
-sqldf.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", ";").save(readPath)
+sqldf_bal.coalesce(1).write.mode("overwrite").format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").option("delimiter", ";").save(readPath)
 
 val name : String = "part-00000"  
 val file_list : Seq[String] = dbutils.fs.ls(readPath).map(_.path).filter(_.contains(name))
@@ -181,6 +230,91 @@ catch {
   case e:FileNotFoundException => println("Error, " + e)
   case e:WorkflowException  => println("Error, " + e)
 }
+  
+  Result
+
+}
+//dbutils.notebook.exit(Result)
+
+
+
+// COMMAND ----------
+
+val query = s"""
+select distinct calendar_yearmonth as partition_name, * from result_source_table 
+""" +
+{if (type_of_data_extract == 1) 
+ s""" 
+ where calendar_yearmonth between 
+ year(date_add(current_date(),-1 * $num_of_days_before_current_date))*100 + month(date_add(current_date(),-1 * $num_of_days_before_current_date))
+ and 
+ year(date_add(current_date(),$num_of_days_after_current_date))*100 + month(date_add(current_date(),$num_of_days_after_current_date))
+ """ 
+ else ""}
+
+// COMMAND ----------
+
+val sqldf = spark.sql(query)
+
+// COMMAND ----------
+
+//export to CAP
+
+// COMMAND ----------
+
+def exportToBlobStorage (type_of_ETL:Int): String = { 
+
+import com.databricks.WorkflowException
+import java.io.FileNotFoundException
+
+var Result = "Failure" 
+val partition_field = "partition_name"
+val export_format = "com.databricks.spark.csv"
+val export_delimiter = Character.toString(7.toChar)
+
+var readPath_ETL = if (type_of_ETL == 0) readPath else if (type_of_ETL == 1) readPath_GBS else null
+var writePath_ETL= if (type_of_ETL == 0) writePath_СAP else if (type_of_ETL == 1) writePath_GBS else null
+
+try {
+  sqldf
+  .coalesce(1)
+  .write.mode("overwrite")
+  .format(export_format)
+  .option("header", "true")
+  .option("inferSchema", "true")
+  .option("delimiter", export_delimiter)
+  .partitionBy(partition_field)
+  .save(readPath_ETL)
+
+  val name : String = "part-00000"   
+  val path_list : Seq[String] = dbutils.fs.ls(readPath_ETL).map(_.path).filter(_.contains(partition_field))
+
+  for (path <- path_list) {
+   var partition_name = path.replace(readPath_ETL + "/" + partition_field + "=", "").replace("/", "")
+   var file_list : Seq[String] = dbutils.fs.ls(path).map(_.path).filter(_.contains(name)) 
+   var read_name =  if (file_list.length >= 1 ) file_list(0).replace(path + "/", "") 
+   var fname = "OPENORDERSTUE_" + partition_name + "_RU_DCD"+ ".csv" 
+   dbutils.fs.mv(read_name.toString , writePath_ETL+"/"+fname) 
+    }
+  dbutils.fs.rm(readPath_ETL , recurse = true) 
+  Result = "Success" 
+  } 
+catch {
+    case e:FileNotFoundException => println("Error, " + e)
+    case e:WorkflowException  => println("Error, " + e)
+  }
+
+  Result
+}
+
+
+
+// COMMAND ----------
+
+val Result = 
+if (type_of_ETL == 0) { if (exportToBlobStorage_Baltika == "Success" &&  exportToBlobStorage(0) == "Success") "Success" else "Failure" }
+else if (type_of_ETL == 1) exportToBlobStorage(1)
+else if (type_of_ETL == 2) { if (exportToBlobStorage_Baltika == "Success" && exportToBlobStorage(0) == "Success" && exportToBlobStorage(1) == "Success") "Success" else "Failure" }
+else "Unexpected parameter"
 
 dbutils.notebook.exit(Result)
-
