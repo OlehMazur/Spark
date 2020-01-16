45795b527424c5b416e2b58ff5f0c5afdb9425a1
OlehMazur <olegmazurbidev@gmail.com>
2020-01-16 09:30:49 +0200

    dcd_notebook_workflow_weekly_update.scala

---
+++ dcd_notebook_workflow_weekly_update.scala
@@ -0,0 +1,49 @@
+// Databricks notebook source
+//7:00
+
+// COMMAND ----------
+
+//dcd_notebook_workflow_open_orders
+
+// COMMAND ----------
+
+//7:30
+
+// COMMAND ----------
+
+//dcd_notebook_workflow_hfa_All
+
+// COMMAND ----------
+
+//8:45
+
+// COMMAND ----------
+
+//dcd_notebook_workflow_indirect
+
+// COMMAND ----------
+
+//10:00
+
+// COMMAND ----------
+
+//dcd_notebook_workflow_df
+
+// COMMAND ----------
+
+//10:30
+
+// COMMAND ----------
+
+//dcd_notebook_workflow_weather
+
+// COMMAND ----------
+
+//11:00
+
+// COMMAND ----------
+
+//dcd_notebook_workflow_event
+
+// COMMAND ----------
+
